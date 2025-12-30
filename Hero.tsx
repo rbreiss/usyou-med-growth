fdafedad3005f719fa4378f002eeaa7c1f1288f1
rbreiss <bruno@reisconsultoria.co>
2025-12-30 12:54:08 -0300

    Update Hero.tsx

--- Hero.tsx
+++ Hero.tsx
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from 'react';
-import { BrandSettings } from '../types';
+import { BrandSettings } from '../types.ts';
 
 interface HeroProps {
   settings: BrandSettings;
